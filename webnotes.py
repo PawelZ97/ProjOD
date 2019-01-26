c6fe17a8eb22c8d4a4a8ad281c0bee4192cd7b3b
PAwelZ97 <pawelzych97@gmail.com>
2019-01-26 15:46:55 +0100

    changed routing , added regex

--- webnotes.py
+++ webnotes.py
@@ -3,30 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 import hashlib
 import binascii
 import os
+import re
 
 app = Flask(__name__)
 
 app.config.from_pyfile('NoSecretThere.cfg')  # for SECRET_KEY
 app.config['SESSION_COOKIE_SECURE'] = False  # debug local
-app.config['SESSION_COOKIE_PATH'] = "/zychp/webnotes"
+app.config['SESSION_COOKIE_PATH'] = "/"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 
-@app.route('/zychp/webnotes/base')
-def baseTest():
+@app.route('/base')
+def base():
     return render_template("base.html")
 
 
-@app.route('/zychp/webnotes/register', methods=['GET', 'POST'])
-def registerTest():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     if (checkUserLogin()):
         return render_template("base.html", message="Już zalogowany.")
     else:
@@ -39,14 +41,14 @@ def registerTest():
             return render_template("register.html")
 
 
-@app.route('/zychp/webnotes/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if (checkUserLogin()):
         return render_template("base.html", message="Już zalogowany.")
     else:
         if request.method == 'POST':
             if doLogin():
-                return redirect("/zychp/webnotes/dashboard")
+                return redirect("/dashboard")
             else:
                 return (render_template("login.html",
                         error="Niepoprawny login lub/i hasło"))
@@ -54,25 +56,25 @@ def login():
             return render_template("login.html")
 
 
-@app.route('/zychp/webnotes/logout')
+@app.route('/logout')
 def logout():
     email = checkUserLogin()
     if (email):
         session.pop('email', None)
-    return redirect('/zychp/webnotes/login')
+    return redirect('/login')
 
-
-@app.route('/zychp/webnotes/dashboard')
+@app.route('/')
+@app.route('/dashboard')
 def dashboard():
     email = checkUserLogin()
     if (email):
         print(email)
         return render_template("dashboard.html", email=email)
     else:
-        return redirect('/zychp/webnotes/login')
+        return redirect('/login')
 
 
-@app.route('/zychp/webnotes/addNote', methods=['GET', 'POST'])
+@app.route('/addNote', methods=['GET', 'POST'])
 def upload():
     email = checkUserLogin()
     if (email):
@@ -84,16 +86,20 @@ def doLogin():
     email = request.form['email']
     password = request.form['password']
 
-    loggingUser = User.query.filter_by(email=email).first()
-    if (loggingUser):
-        if(verify_password(loggingUser.password, password)):
-            session['email'] = email
-            return email
+    if(checkEmail(email) and checkPassword(password)):
+        loggingUser = User.query.filter_by(email=email).first()
+        if (loggingUser):
+            if(verify_password(loggingUser.password, password)):
+                session['email'] = email
+                return email
+            else:
+                print("Wrong password")
+                return False
         else:
-            print("Wrong password")
+            print("User do not exist")
             return False
     else:
-        print("User do not exist")
+        print("Wrong input")
         return False
 
 
@@ -102,18 +108,22 @@ def registerNewUser():
     password = request.form['password']
     repassword = request.form['repassword']
 
-    if((User.query.filter_by(email=email).first()) is None):
-        if(password == repassword):
-            createdUser = User(email=email, password=hash_password(password))
-            db.session.add(createdUser)
-            db.session.commit()
-            print("Register succesful")
-            return True
+    if(checkEmail(email) and checkPassword(password) and checkPassword(repassword)):
+        if((User.query.filter_by(email=email).first()) is None):
+            if(password == repassword):
+                createdUser = User(email=email, password=hash_password(password))
+                db.session.add(createdUser)
+                db.session.commit()
+                print("Register succesful")
+                return True
+            else:
+                print("Passwords not match")
+                return False
         else:
-            print("Passwords not match")
+            print("User exist")
             return False
     else:
-        print("User exist")
+        print("Wrong input")
         return False
 
 
@@ -143,3 +153,11 @@ def verify_password(stored_password, provided_password):
                                   100000)
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
     return pwdhash == stored_password
+
+
+def checkEmail(email):
+    return re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)
+
+
+def checkPassword(password):
+    return re.match('^[!-~]*$', password)
